a91533cab2d063147dd7c53ddcb1886c9f29bf90
LukeDeWaal <lr.de.waal.01@gmail.com>
2019-06-09 01:19:45 +0200

    Further wing updates

--- src/WingTool.py
+++ src/WingTool.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from typing import Union
 from NumericalTools import linear_interpolation_nearest_neighbour
+from AirFoilTool import FiveDigitNACA, FourDigitNACA
 
 class Wing(object):
 
-    def __init__(self, halfspan: Union[int, float]):
+    def __init__(self, halfspan: Union[int, float], n_steps: int = 100):
 
         # Initialize all parameters defining the wing
         self.b = halfspan
@@ -21,126 +22,242 @@ class Wing(object):
         self.airfoil_distribution = None
         self.dihedral_distribution = None
 
+        # Final Coordinates of the wing
+        self.__n_steps = n_steps
+        self.datapoints = np.zeros((3, self.__n_steps))
+
+
+    """
+    Detail-Level Methods go below here
+    """
     @staticmethod
     def __calculate_MAC(c: np.array):
         return np.average(c)
 
+    """
+    The Following 4 Methods are for the correct assignment of the data describing the wing.
+    All data is eventually stored in dictionaries but can be inputted as:
+        - Integers and floats for constant values along span
+        - Lists or Arrays for custom definitions of the variable
+        - Callables as a different way of customizing the variable
+        - Dictionaries which contain the y coordinates and their variables in a list or array 
+          can also be inputted with the correct key
+    """
     @staticmethod
-    def __number_input_allocation(number: int or float, span: int or float, key: str,target: dict or None, n_steps: int = 100):
+    def __number_input_allocation(number: int or float or str, span: int or float, key: str,target: dict or None, n_steps: int, airfoil: bool = False):
 
         if target is None:
             target = {}
 
-        target['y'] = np.linspace(0, span, n_steps)
-        target[key] = np.array([number for _ in target['y']])
+        if airfoil is False:
+            target['y'] = np.linspace(0, span, n_steps)
+            target[key] = np.array([number for _ in target['y']])
+
+        elif airfoil is True:
+            number = str(number)
+
+            if len(number) == 4:
+                coordinates = FourDigitNACA(number).calculate_coordinates(cosine_spacing=True)
+
+            elif len(number) == 5:
+                coordinates = FiveDigitNACA(number).calculate_coordinates(cosine_spacing=True)
+
+            else:
+                raise ValueError("Unexpected Input")
+
+            # TODO: This is an incorrect implementation, correct the way coordinates are saved
+            target['x'] = np.concatenate((coordinates['XU'], coordinates['XL'][::-1]))
+            target['y'] = np.linspace(0, span, n_steps)
+            target['z'] = np.concatenate((coordinates['YU'], coordinates['YL'][::-1]))
+
+
 
     @staticmethod
-    def __array_input_allocation(array: list or np.array, span: int or float, key: str, target: dict or None, n_steps: int = 100):
+    def __array_input_allocation(array: list or np.array, key: str, target: dict or None, airfoil: bool = False):
 
         if target is None:
             target = {}
 
-        if type(array) == list:
-            array = np.array(array)
+        if airfoil is False:
+
+            if type(array) == list:
+                array = np.array(array)
+
+            if array.shape[0] > array.shape[1]:
+                target[key] = array[:, 0]
+                target["y"] = array[:, 1]
+
+            elif array.shape[0] < array.shape[1]:
+                target[key] = array[0, :]
+                target["y"] = array[1, :]
+
+        elif airfoil is True:
+
+            if type(array[0][0]) == str:
+                for number in array[0]:
+                    number = str(number)
+
+                    if len(number) == 4:
+                        coordinates = FourDigitNACA(number).calculate_coordinates(cosine_spacing=True)
+
+                    elif len(number) == 5:
+                        coordinates = FiveDigitNACA(number).calculate_coordinates(cosine_spacing=True)
+
+                    else:
+                        raise ValueError("Unexpected Input")
+
+            elif type(array[1][0]) == str:
+                pass
+            #TODO: Lots to finish here
+
+
+            if array.shape[0] > array.shape[1]:
+                target["x"] = array[:, 0]
+                target["y"] = array[:, 1]
+                target["z"] = array[:, 2]
+
+            elif array.shape[0] < array.shape[1]:
+                target["x"] = array[0, :]
+                target["y"] = array[1, :]
+                target["z"] = array[2, :]
+
+    @staticmethod
+    def __callable_input_allocation(function: callable, span: int or float, key: str, target: dict or None, n_steps: int):
+        
+        if target is None:
+            target = {}
 
         target['y'] = np.linspace(0, span, n_steps)
+        target[key] = np.array([function(yi) for yi in target['y']])
+        
+    @staticmethod
+    def __dictionary_input_allocation(dictionary: dict, key: str, target: dict or None):
+        
+        if target is None:
+            target = {}
 
-        if array.shape[0] > array.shape[1]:
-            target = {
-                key: array[:, 0],
-                "y": array[:, 1]
-            }
+        target['y'] = dictionary['y']
+        target[key] = dictionary[key]
 
-        elif array.shape[0] < array.shape[1]:
-            target = {
-                key: array[0, :],
-                "y": array[1, :]
-            }
+    """
+    These methods will be used by the user to create their wing
+    """
+    def set_airfoil(self, airfoil: Union[int, str, callable, list, np.array, dict]):
+        
+        airfoiltype = type(airfoil)
 
+        # TODO: Make airfoil selection process more modular
+        if airfoiltype in [int, str]:
+            airfoil = str(airfoil) if airfoiltype is int else airfoil
+            self.__number_input_allocation(airfoil, self.b, 'airfoil', self.airfoil_distribution, self.__n_steps, airfoil=True)
 
-    def set_sweep(self, sweep: Union[int, float, callable, list, np.array, dict], n_steps: int = 100):
+        elif airfoiltype in [list, np.array]:
+            self.__array_input_allocation(airfoil, 'airfoil', self.airfoil_distribution, airfoil=True)
+
+        elif airfoiltype is callable(airfoil):
+            self.__callable_input_allocation(airfoil, self.b, 'airfoil', self.airfoil_distribution, self.__n_steps)
+
+        elif airfoiltype is dict:
+            self.__dictionary_input_allocation(airfoil, 'airfoil', self.airfoil_distribution)
+
+        else:
+            raise TypeError("Invalid Input")
+    
+    def set_twist(self, twist: Union[int, float, callable, list, np.array, dict]):
+        
+        twisttype = type(twist)
+
+        if twisttype in [int, float]:
+            self.__number_input_allocation(twist, self.b, 'twist', self.twist_distribution, self.__n_steps)
+
+        elif twisttype in [list, np.array]:
+            self.__array_input_allocation(twist, 'twist', self.twist_distribution)
+
+        elif twisttype is callable:
+            self.__callable_input_allocation(twist, self.b, 'twist', self.twist_distribution, self.__n_steps)
+
+        elif twisttype is dict:
+            self.__dictionary_input_allocation(twist, 'twist', self.twist_distribution)
+
+        else:
+            raise TypeError("Invalid Input")
+    
+    def set_dihedral(self, dihedral: Union[int, float, callable, list, np.array, dict]):
+        
+        dihedraltype = type(dihedral)
+        
+        if dihedraltype in [int, float]:
+            self.__number_input_allocation(dihedral, self.b, 'dihedral', self.dihedral_distribution, self.__n_steps)
+
+        elif dihedraltype in [list, np.array]:
+            self.__array_input_allocation(dihedral, 'dihedral', self.dihedral_distribution)
+
+        elif dihedraltype is callable:
+            self.__callable_input_allocation(dihedral, self.b, 'dihedral', self.dihedral_distribution, self.__n_steps)
+
+        elif dihedraltype is dict:
+            self.__dictionary_input_allocation(dihedral, 'dihedral', self.dihedral_distribution, )
+
+        else:
+            raise TypeError("Invalid Input")
+        
+    def set_sweep(self, sweep: Union[int, float, callable, list, np.array, dict]):
 
         sweeptype = type(sweep)
-
+    
         if sweeptype in [int, float]:
-            self.sweep_distribution = {
-                "sweep": np.array([sweep for _ in np.linspace(0, self.b, n_steps)]),
-                "y": np.linspace(0, self.b, n_steps)
-            }
+            self.__number_input_allocation(sweep, self.b, 'sweep', self.sweep_distribution, self.__n_steps)
 
         elif sweeptype in [list, np.array]:
-            sweep = np.array(sweep) if sweeptype is list else sweep
-
-            if sweep.shape[0] > sweep.shape[1]:
-                self.sweep_distribution = {
-                    "sweep": sweep[:, 0],
-                    "y": sweep[:, 1]
-                }
-
-            elif sweep.shape[0] < sweep.shape[1]:
-                self.sweep_distribution = {
-                    "sweep": sweep[0, :],
-                    "y": sweep[1, :]
-                }
+            self.__array_input_allocation(sweep, 'sweep', self.sweep_distribution, )
 
         elif sweeptype is callable:
+            self.__callable_input_allocation(sweep, self.b, 'sweep', self.sweep_distribution, self.__n_steps)
+            
+        elif sweeptype is dict:
+            self.__dictionary_input_allocation(sweep, 'sweep', self.sweep_distribution)
+            
+        else:
+            raise TypeError("Invalid Input")
 
-            self.sweep_distribution = {
-                "sweep":
-            }
-
-    def set_chord(self, c: Union[int, float, callable, list, np.array, dict], n_steps: int = 100):
+    def set_chord(self, c: Union[int, float, callable, list, np.array, dict]):
 
         ctype = type(c)
 
         if ctype in [int, float]:
             self.cr = c
             self.ct = c
-            self.chord_distribution = {
-                "c": np.array([c for _ in np.linspace(0, self.b, n_steps)]),
-                "y": np.linspace(0, self.b, n_steps)
-            }
             self.taper = 1
+            self.__number_input_allocation(c, self.b, 'chord', self.chord_distribution, self.__n_steps)
 
         elif ctype in [list, np.array]:
-            c = np.array(c) if ctype is list else c
-            self.cr = c[0]
-            self.ct = c[-1]
 
-            if c.shape[0] >= c.shape[1]:
-                self.chord_distribution = {
-                    "c": c[:, 0],
-                    "y": c[:, 1]
-                }
-            elif c.shape[0] < c.shape[1]:
-                self.chord_distribution = {
-                    "c": c[0, :],
-                    "y": c[1, :]
-                }
-
+            self.__array_input_allocation(c, 'chord', self.chord_distribution)
+            self.cr = self.chord_distribution['chord'][0]
+            self.ct = self.chord_distribution['chord'][-1]
             self.taper = self.ct/self.cr
 
         elif ctype == callable:
             self.cr = c(0)
             self.ct = c(self.b)
-            self.chord_distribution = {
-                "c": np.array([c(yi) for yi in np.linspace(0, self.b, n_steps)]),
-                "y": np.linspace(0, self.b, n_steps)
-            }
-
             self.taper = self.ct/self.cr
+            self.__callable_input_allocation(c, self.b, 'chord', self.chord_distribution, self.__n_steps)
 
         elif ctype == dict:
-            self.cr = c["c"][0]
-            self.ct = c["c"][-1]
-            self.chord_distribution = {
-                "c": np.array(c["c"]),
-                "y": np.array(c["y"])
-            }
-
+            self.__dictionary_input_allocation(c, 'chord', self.chord_distribution)
+            self.cr = self.chord_distribution['chord'][0]
+            self.ct = self.chord_distribution['chord'][0]
             self.taper = self.ct/self.cr
 
         else:
             raise TypeError("Invalid Input")
 
-        self.MAC = self.__calculate_MAC(self.chord_distribution['c'])
+        self.MAC = self.__calculate_MAC(self.chord_distribution['chord'])
+
+
+    """
+    Once all desired variables are defined, the wing will have to be 'assembled'. 
+    Aka. The 3D coordinates will have to be calculated.
+    """
+    def construct(self, double_sided: bool = False):
+        pass
