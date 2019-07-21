12c76b01b525fc0a991c3edb7ac5be9f8d478498
LukeDeWaal <lr.de.waal.01@gmail.com>
2019-07-21 18:27:15 +0200

    GUI Editing Additions v1

--- src/Visuals.py
+++ src/Visuals.py
@@ -16,6 +16,102 @@ from WingTool import Wing
 
 LARGE_FONT = ("Verdana", 12)
 
+ROOT_GEO = f"{700}x{600}+{200}+{200}"
+EDITOR_GEO = f"{300}x{500}+{900}+{200}"
+
+
+# TODO: Create editors
+class EditorWindow(tk.Toplevel):
+
+    def __init__(self, root, name):
+
+        super().__init__(root)
+        self.title(name)
+        self.geometry(EDITOR_GEO)  # TODO: Make modular with main window
+
+        self.menubar = tk.Menu(self.master)
+        self.config(menu=self.menubar)
+
+        # File Menu
+        fileMenu = tk.Menu(self.menubar)
+        fileMenu.config(tearoff=False)
+        fileMenu.add_command(label="New", command=lambda: None)
+        fileMenu.add_command(label="Open", command=lambda: None)
+        fileMenu.add_command(label="Save", command=lambda: None)
+        fileMenu.add_command(label="Exit", command=lambda: self.wm_withdraw())
+        self.menubar.add_cascade(label="File", menu=fileMenu)
+
+        self.input_text = ""
+        self.text = tk.Text(self, height=30, width=30)
+        self.text.grid(row=0, column=0)
+
+        self.text.insert(
+            tk.INSERT,
+            "0: 0\n1: 0.1\n2: 0.3"
+        )
+
+        self.btn_frame = tk.Frame(self)
+        self.btn_frame.grid(row=0, column=1)
+
+        self.btn_frame.grid_rowconfigure(0, weight=1)
+        self.btn_frame.grid_rowconfigure(1, weight=1)
+        self.btn_frame.grid_columnconfigure(0, weight=1)
+
+        self.apply_btn = tk.Button(
+            self.btn_frame,
+            text='Apply',
+            command=lambda: None
+        )
+        self.apply_btn.grid(
+            row=0,
+            column=0,
+            sticky='n',
+        )
+
+        self.clear_btn = tk.Button(
+            self.btn_frame,
+            text='Clear',
+            command=lambda: None
+        )
+        self.clear_btn.grid(
+            row=1,
+            column=0,
+            sticky='n',
+        )
+
+    def __retrieve_input(self):
+        self.input_text = self.text.get("1.0", tk.END)
+
+    @staticmethod
+    def __y_parser(txt):
+
+        if "-" in txt:
+            `
+
+    @staticmethod
+    def __val_parser(txt):
+        pass
+
+    def __parser(self):
+
+        y = []
+        val = []
+
+        lines = self.input_text.split("\n")
+
+
+    def __apply(self):
+        pass
+
+    def __open(self):
+        pass
+
+    def __save(self):
+        pass
+
+    def __new(self):
+        pass
+
 
 class WingEditor(tk.Tk):
 
@@ -31,6 +127,7 @@ class WingEditor(tk.Tk):
         self.wing = Wing(n_steps=self.n)
 
         super().__init__(*args, **kwargs)
+        self.geometry(ROOT_GEO)
 
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
@@ -45,9 +142,9 @@ class WingEditor(tk.Tk):
 
         self.editor_container = tk.Frame(self)
         self.editor_container.grid(row=0, column=1)
-        self.editor_container.columnconfigure(1, weight=1)
-        self.editor_container.rowconfigure(1, weight=1)
-        self.editor_container.rowconfigure(2, weight=1)
+        self.editor_container.grid_columnconfigure(1, weight=1)
+        self.editor_container.grid_rowconfigure(0, weight=1)
+        self.editor_container.grid_rowconfigure(1, weight=1)
 
         # Menu Bar Creation
         self.menubar = tk.Menu(self.master)
@@ -56,7 +153,7 @@ class WingEditor(tk.Tk):
         # File Menu
         fileMenu = tk.Menu(self.menubar)
         fileMenu.config(tearoff=False)
-        fileMenu.add_command(label="Exit", command=lambda: None)
+        fileMenu.add_command(label="Exit", command=lambda: quit())
         self.menubar.add_cascade(label="File", menu=fileMenu)
 
         """
@@ -81,13 +178,13 @@ class WingEditor(tk.Tk):
         """
         self.parameter_label_texts = [
             'Span',
+            'Span Steps',
+            'Airfoil Steps',
             'Airfoils',
             'Chord',
             'Sweep',
             'Twist',
             'Dihedral',
-            'Span Steps',
-            'Airfoil Steps'
         ]
 
         self.parameter_labels = {}
@@ -111,19 +208,45 @@ class WingEditor(tk.Tk):
                 pady=SPACING[1]
             )
 
-            self.parameter_entries[label] = ttk.Entry(
-                                                self.editor_container,
-                                                textvariable=self.parameter_variables[label]
-                                            )
+            if 0 <= idx <= 2:
+                self.parameter_entries[label] = ttk.Entry(
+                                                    self.editor_container,
+                                                    textvariable=self.parameter_variables[label]
+                                                )
 
-            self.parameter_variables[label].set(0)
-            self.parameter_entries[label].grid(
-                row=idx,
-                column=1,
-                sticky='N',
-                padx=SPACING[0],
-                pady=SPACING[1]
-            )
+                self.parameter_variables[label].set(0)
+                self.parameter_entries[label].grid(
+                    row=idx,
+                    column=1,
+                    sticky='N',
+                    padx=SPACING[0],
+                    pady=SPACING[1]
+                )
+
+            else:
+                if label == 'Airfoils':
+                    self.parameter_entries[label] = ttk.Button(
+                        self.editor_container,
+                        text="Edit",
+                        command=self.__open_airfoil_editor,
+                        state=tk.DISABLED
+                    )
+                else:
+                    self.parameter_entries[label] = ttk.Button(
+                        self.editor_container,
+                        text="Edit",
+                        command=lambda: None,
+                        state=tk.DISABLED
+                    )
+
+                self.parameter_variables[label].set(0)
+                self.parameter_entries[label].grid(
+                    row=idx,
+                    column=1,
+                    sticky='NSEW',
+                    padx=SPACING[0],
+                    pady=SPACING[1]
+                )
 
         self.editor_container.columnconfigure(0, weight=1)
         self.editor_container.columnconfigure(1, weight=1)
@@ -163,6 +286,38 @@ class WingEditor(tk.Tk):
         for idx in range(len(self.parameter_labels)+1):
             self.editor_container.rowconfigure(idx, weight=1)
 
+        self.parameter_variables['Span'].trace_add("write", lambda name, index, mode: self.__entry_box_change())
+        self.parameter_variables['Span Steps'].trace_add("write", lambda name, index, mode: self.__entry_box_change())
+        self.parameter_variables['Airfoil Steps'].trace_add("write",
+                                                            lambda name, index, mode: self.__entry_box_change())
+
+    def __entry_box_change(self):
+        """
+        Keeps track of the the span and discretization values
+        and blocks further editing if necessary
+        """
+
+        if self.parameter_variables['Span'].get() == "" or \
+                self.parameter_variables['Span Steps'].get() == "" or \
+                self.parameter_variables['Airfoil Steps'].get() == "":
+            for label in self.parameter_label_texts[3:]:
+                self.parameter_entries[label].config(state=tk.DISABLED)
+
+        elif int(self.parameter_variables['Span'].get()) > 0 and \
+                int(self.parameter_variables['Span Steps'].get()) > 0 \
+                and int(self.parameter_variables['Airfoil Steps'].get()) > 0:
+
+            for label in self.parameter_label_texts[3:]:
+                self.parameter_entries[label].config(state=tk.NORMAL)
+
+        else:
+            for label in self.parameter_label_texts[3:]:
+                self.parameter_entries[label].config(state=tk.DISABLED)
+
+    def __open_airfoil_editor(self):
+
+        editor = EditorWindow(self, 'Airfoils')
+
     def __extract_inputs(self):
 
         input_values = {}
@@ -179,6 +334,7 @@ class WingEditor(tk.Tk):
                     pass
 
             else:
+                #TODO: Fix Edit windows
                 try:
                     value = int(value)
 
@@ -194,11 +350,10 @@ class WingEditor(tk.Tk):
         arr = self.wing.data_container.get_array()
 
         self.plot3d.scatter(arr[0, :], arr[1, :], arr[2, :], c='r', marker='o')
+        self.wing.axisEqual3D(self.plot3d)
         self.plot3d.set_xlabel('X [m]')
         self.plot3d.set_ylabel('Y [m]')
         self.plot3d.set_zlabel('Z [m]')
-
-        self.wing.axisEqual3D(self.plot3d)
 
     def __update_plot(self):
 
